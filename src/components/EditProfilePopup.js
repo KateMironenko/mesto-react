5341fdc87ce5f08907b517022b97806c8aea513f
Kate Miranenka <kateway97@gmail.com>
2020-11-16 01:26:22 +0300

    editProfile fix

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -12,7 +12,7 @@ function EditProfilePopup (props){
   React.useEffect(() => {
       setName(currentUser.name);
       setDescription(currentUser.about);
-    }, [currentUser]); 
+    }, [currentUser, isOpen]); 
 
 
 function handleSubmit(e) {
